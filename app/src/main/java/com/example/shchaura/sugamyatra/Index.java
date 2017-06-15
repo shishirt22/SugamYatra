321a155181dda9cdc7789eed593f7a8f71dae7fe
sagarkaniche <sagarkaniche007@gmail.com>
2017-06-15 15:10:05 +0530

    Yo Commit

--- app/src/main/java/com/example/shchaura/sugamyatra/Index.java
+++ app/src/main/java/com/example/shchaura/sugamyatra/Index.java
@@ -5,8 +5,10 @@ import android.os.Bundle;
 
 public class Index extends AppCompatActivity {
 
+    int hot = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
     }
+}
